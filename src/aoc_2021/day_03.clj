2e4d85f80ba53febf648732e07f72651b0798533
Amith George <strider.am@gmail.com>
2022-04-21 01:51:58 +0530

    add docstrings

--- src/aoc_2021/day_03.clj
+++ src/aoc_2021/day_03.clj
@@ -36,15 +36,28 @@
        (mapv binary-str->vec)))
 
 (defn digit-frequencies
+  "Given input like 
+    [[1 0 1]
+      [1 0 0]]
+   The output will be 
+     [{1 2} {0 2} {0 1, 1 1}]
+   Each element of the output is a map with maximum two keys, 0 and 1.
+    The value of each key is the frequency of that digit in that bit-location 
+    in all the binary numbers. In the most-significant bit-location, 
+    the digit 1 appears twice, hence the output contains the map {1 2}"
   [binary-numbers]
   (reduce
    (fn [acc binary-num]
      (map (fn [m digit]
-            (update m digit (fnil inc 0))) acc binary-num))
+            (update m digit (fnil inc 0)))
+          acc
+          binary-num))
    (vec (repeat (count (first binary-numbers)) {}))
    binary-numbers))
 
 (defn higher-frequency-digit
+  "Given a map of digit frequences, returns the digit 
+  that is more frequent. Eg for input {0 3, 1 4}, return 1"
   [m]
   (if (> (get m 0 0)
          (get m 1 0))
@@ -52,6 +65,8 @@
     1))
 
 (defn lower-frequency-digit
+  "Given a map of digit frequences, returns the digit 
+  that is less frequent. Eg for input {0 3, 1 4}, return 0"
   [m]
   (if (< (get m 0 0)
          (get m 1 0))
